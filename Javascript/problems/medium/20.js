fc282e690f7a3a0ffb914bae1c787f270e0c5d8e
Vel1Zy <adriel.ichwan@gmail.com>
2025-02-15 17:53:45 +0700

    20 21 - problem

--- Javascript/problems/medium/20.js
+++ Javascript/problems/medium/20.js
@@ -45,3 +45,14 @@ function rot13(message){
     return char;
   }).join('');
 }
+
+// solution 5
+
+function rot13(message){
+    //your code here
+    return (message + '')
+      .replace(/[a-z]/gi, function(s) {
+        return String.fromCharCode(s.charCodeAt(0) + (s.toLowerCase() < 'n' ? 13 : -13));
+      });
+  
+}
